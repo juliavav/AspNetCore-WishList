1f7c94d5b218206c131528e0b6bb6d975a1e7b17
Юлия Валентинова <julia.vav@gmail.com>
2019-02-26 18:36:09 +0300

    added: third task name changes 2

--- WishList/Data/ApplicationDBContext.cs
+++ WishList/Data/ApplicationDBContext.cs
@@ -7,12 +7,11 @@ using WishList.Models;
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
         ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
         }
     }
 }
