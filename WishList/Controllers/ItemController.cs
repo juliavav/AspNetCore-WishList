9f0f83866681f50f88cbba25454dea301de2ceae
Юлия Валентинова <julia.vav@gmail.com>
2019-02-26 19:53:19 +0300

    added: fourth task

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Create()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
