5dc50197f5a4cddee573ff90ace70adb7621d064
Юлия Валентинова <julia.vav@gmail.com>
2019-02-26 18:51:14 +0300

    Update ApplicationDbContext.cs

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using WishList.Models;
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
         ApplicationDbContext(DbContextOptions options) : base(options)
